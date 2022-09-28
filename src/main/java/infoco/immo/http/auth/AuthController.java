4e02e02810438dc8c412590b47662259e0b1dac3
tim <timoth.david@gmail.com>
2022-09-28 15:31:07 +0200

    secure creation of creation of account

--- src/main/java/infoco/immo/http/auth/AuthController.java
+++ src/main/java/infoco/immo/http/auth/AuthController.java
@@ -8,6 +8,7 @@ import infoco.immo.http.user.dto.LoginUserDTO;
 import infoco.immo.usecase.user.Token;
 import infoco.immo.http.user.dto.CreateUserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,7 @@ public class AuthController {
     UserService userService;
 
 
+    @Profile("!prod")
     @PostMapping
     public ResponseEntity create(@Valid  @RequestBody  CreateUserDTO createUserDTO) {
         User user = User.builder()
